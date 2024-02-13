67f11d4c070e053c5354ac968de9e4d9e7e75ea8
nlopez <nelson.lopez1@ibm.com>
2024-02-13 08:05:35 -0500

    added new STATIC demo pgm

---
+++ common-source/Common-Static-Calls/SUB1.cbl
@@ -0,0 +1,9 @@
+       ID DIVISION.
+       PROGRAM-ID. STATIC1.
+      * Demo sample static call in seperate repo 
+      * mapped this folder in linkSearch 
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.          
+           DISPLAY 'STATC1 here v1'.
